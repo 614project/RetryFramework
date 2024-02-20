313d478286d4b24bad82004e09302561bc8fa4e7
614project <71429443+614project@users.noreply.github.com>
2024-02-21 03:37:57 +0900

    글자 추가

--- Error/logger.cs
+++ Error/logger.cs
@@ -2,11 +2,20 @@
 
 public class Logger
 {
-    Queue<string> queue = new();
-    public string? Pop() => queue.Count is 0 ? null : queue.Dequeue();
-    internal void Push(string push) => queue.Enqueue(push);
+    Queue<(string,DateTime)> queue = new();
+    public (string message, DateTime time)? Pop() => queue.Count is 0 ? null : queue.Dequeue();
+    public bool Pop(out string error,out DateTime time)
+    {
+        error = string.Empty;
+        time = default;
+        if (queue.Count is 0) return false;
+        (error,time) = queue.Dequeue();
+        return true;
+    }
+    internal void Push(string push) => queue.Enqueue((push,DateTime.Now));
 
-    public static implicit operator string?(Logger logger) => logger.Pop();
+    public static implicit operator string?(Logger logger) => logger.Pop()?.message;
+    public static implicit operator (string message, DateTime time)?(Logger logger) => logger.Pop();
     public static Logger operator +(Logger logger,string message)
     {
         logger.Push(message);
---
+++ Font/fromfile.cs
@@ -0,0 +1,31 @@
+﻿using RetryFramework.SDL2;
+
+namespace RetryFramework;
+
+public partial class Font
+{
+    public class FromFile : RetryFont
+    {
+        public FromFile(string? path = null)
+        {
+            _path = path ?? DefaultPath;
+        }
+        public virtual string Path
+        {
+            get => _path; set
+            {
+                _path = value;
+                if (IsLoad)
+                {
+                    Release();
+                    Prepare();
+                }
+            }
+        }
+        public override void Prepare()
+        {
+            if ((_ptr = SDL_ttf.TTF_OpenFont(_path, _size)) == IntPtr.Zero) error_push();
+        }
+        private string _path;
+    }
+}
